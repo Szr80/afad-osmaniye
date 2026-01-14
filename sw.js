87fb1e7a6fd5479397190a19c8a2dab82e29c768
Szr80 <sezerytbe80@gmail.com>
2026-01-14 21:04:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'afad-vaka-v3';
+const CACHE_NAME = 'afad-vaka-cache-v1';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
@@ -12,7 +12,7 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
 
-// Arka Plan Firebase Dinleme
+// Firebase Arka Plan Dinleme
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js');
 
@@ -22,13 +22,18 @@ firebase.initializeApp(firebaseConfig);
 firebase.database().ref('ortakKanal').on('value', (snapshot) => {
     const data = snapshot.val();
     if (data && data.metin && data.metin !== "KOMUTA BEKLENİYOR...") {
-        self.registration.showNotification('🚨 YENİ VAKA BİLDİRİMİ', {
+        self.registration.showNotification('🚨 YENİ VAKA İHBARI', {
             body: data.metin,
             icon: 'https://upload.wikimedia.org/wikipedia/tr/e/ee/Afet_ve_Acil_Durum_Y%C3%B6netimi_Ba%C5%9Fanl%C4%B1%C4%9F%C4%B1_logosu.png',
-            vibrate: [500, 200, 500, 200, 500],
+            vibrate: [500, 200, 500],
             tag: 'vaka-bildirim',
             renotify: true,
             requireInteraction: true
         });
     }
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/'));
 });
