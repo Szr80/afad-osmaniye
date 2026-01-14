0712732489c8e0b4d05cc52a65efa1b3d868c41d
Szr80 <sezerytbe80@gmail.com>
2026-01-14 20:58:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'afad-vaka-v2';
+const CACHE_NAME = 'afad-vaka-v3';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
@@ -9,11 +9,10 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    // PWA kurulumu için boş fetch listener yeterlidir
     event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
 
-// Arka Plan Bildirimleri
+// Arka Plan Firebase Dinleme
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js');
 
@@ -23,11 +22,11 @@ firebase.initializeApp(firebaseConfig);
 firebase.database().ref('ortakKanal').on('value', (snapshot) => {
     const data = snapshot.val();
     if (data && data.metin && data.metin !== "KOMUTA BEKLENİYOR...") {
-        self.registration.showNotification('🚨 1 YENİ VAKA!', {
+        self.registration.showNotification('🚨 YENİ VAKA BİLDİRİMİ', {
             body: data.metin,
             icon: 'https://upload.wikimedia.org/wikipedia/tr/e/ee/Afet_ve_Acil_Durum_Y%C3%B6netimi_Ba%C5%9Fanl%C4%B1%C4%9F%C4%B1_logosu.png',
-            vibrate: [500, 200, 500],
-            tag: 'vaka-ihbar',
+            vibrate: [500, 200, 500, 200, 500],
+            tag: 'vaka-bildirim',
             renotify: true,
             requireInteraction: true
         });
