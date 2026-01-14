f2cbd6f87c9642992b833b63e1828132fadea0f9
Szr80 <sezerytbe80@gmail.com>
2026-01-14 20:44:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+const CACHE_NAME = 'vaka80-v1';
+const assets = ['./index.html', './manifest.json'];
+
+// Yükleme sırasında dosyaları önbelleğe al
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Firebase Bildirimleri ve Veri Dinleme
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js');
 
@@ -5,22 +21,15 @@ const firebaseConfig = { databaseURL: "https://ana-uygulama-f22cb-default-rtdb.a
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-// Arka planda vaka dinleme
 db.ref('ortakKanal').on('value', (snapshot) => {
-    const data = snapshot.val();
-    if (data && data.metin && data.metin !== "KOMUTA BEKLENİYOR...") {
-        self.registration.showNotification('🚨 1 YENİ VAKA VAR!', {
-            body: data.metin,
-            icon: 'https://upload.wikimedia.org/wikipedia/tr/e/ee/Afet_ve_Acil_Durum_Y%C3%B6netimi_Ba%C5%9Fanl%C4%B1%C4%9F%C4%B1_logosu.png',
-            vibrate: [600, 200, 600, 200, 600],
-            tag: 'afad-ihbar',
-            renotify: true,
-            requireInteraction: true
-        });
-    }
-});
-
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow('/'));
+  const data = snapshot.val();
+  if (data && data.metin && data.metin !== "KOMUTA BEKLENİYOR...") {
+    self.registration.showNotification('🚨 YENİ VAKA!', {
+      body: data.metin,
+      icon: 'https://upload.wikimedia.org/wikipedia/tr/e/ee/Afet_ve_Acil_Durum_Y%C3%B6netimi_Ba%C5%9Fanl%C4%B1%C4%9F%C4%B1_logosu.png',
+      vibrate: [500, 200, 500],
+      tag: 'vaka-bildirim',
+      renotify: true
+    });
+  }
 });
