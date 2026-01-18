0b07ce1a0c670e363c45363bc35962fec8c81e1f
Szr80 <sezerytbe80@gmail.com>
2026-01-18 09:34:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// sw.js - Kesintisiz Arka Plan ve Otomatik Tetikleme Motoru
+// sw.js - AFAD Arka Plan Hizmet Motoru
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
@@ -7,12 +7,13 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
+// Arka planda vaka sinyali geldiğinde tetiklenir
 self.addEventListener('push', function(event) {
     const options = {
         body: event.data ? event.data.text() : 'ACİL: Yeni AFAD vaka kaydı düşmüştür!',
         icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
         badge: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
-        vibrate: [500, 110, 500, 110, 500, 110, 500],
+        vibrate: [500, 110, 500, 110, 500],
         requireInteraction: true,
         tag: 'afad-vaka-kesin',
         renotify: true,
@@ -24,6 +25,7 @@ self.addEventListener('push', function(event) {
     );
 });
 
+// Bildirime tıklanınca uygulamayı uykudan uyandırır
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(
