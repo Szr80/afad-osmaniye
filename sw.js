1c32b7b9b245e6b47224da41be2824c42de499af
Szr80 <sezerytbe80@gmail.com>
2026-03-19 13:58:42 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -7,15 +7,15 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// Bildirimleri Yakala
+// Arka planda vaka bildirimlerini yakalar
 self.addEventListener('push', (event) => {
-    const data = event.data ? event.data.json() : { title: 'AFAD GÖREV', body: 'Yeni vaka bildirimi!' };
+    const data = event.data ? event.data.json() : { title: 'AFAD GÖREV', body: 'Yeni vaka bildirimi var!' };
     const options = {
         body: data.body,
         icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
         badge: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
         vibrate: [500, 200, 500, 200, 500],
-        requireInteraction: true,
+        requireInteraction: true, // Kullanıcı kapatana kadar ekranda kalır
         data: { url: '/' }
     };
     event.waitUntil(self.registration.showNotification(data.title, options));
